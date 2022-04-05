8d99d3a70e1baa67aa92a8cda2e59cec7be18e78
Ricardo Forte Pace <tatopace@hotmail.com>
2022-04-05 15:36:18 -0300

    typing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,20 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import "channels";
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import "channels"
+Rails.start();
+Turbolinks.start();
 
-Rails.start()
-Turbolinks.start()
+var i = 0;
+var txt = "It's me, Ricardo!_"; /* The text */
+var speed = 200; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
